24bb28589cf3edf4b0f5945fc50ec24905d6ed42
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 17:19:19 -0300

    Sistema

---
+++ Sistema Biblioteca/Querys/LivroAutor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LivroAutor]
+(
+	[Livro] INT NOT NULL,
+	[Autor] int not null, 
+    CONSTRAINT [FK_LivroAutor_Livros] FOREIGN KEY ([Livro]) REFERENCES [Livros]([Id]), 
+    CONSTRAINT [FK_LivroAutor_Autores] FOREIGN KEY ([Autor]) REFERENCES [Autores]([Id])
+
+)
--- Sistema Biblioteca/Querys/Livros.sql
+++ Sistema Biblioteca/Querys/Livros.sql
@@ -1,21 +1,21 @@
-﻿CREATE TABLE [dbo].[Livro]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[Registro] int not null,
-	[Titulo] varchar(1200) not null,
-	[Isbn] varchar(15) not null,
-	[Genero] int not null,
-	[Editora] int not null,
-	[Sinopse] NVARCHAR(MAX) null,
-	[Observaçoes] varchar(1000) null,
-	[Ativo] bit not null,
-	[UsuInc] int not null,
-	[UsuAlt] int not null,
-	[DatInc] datetime not null,
-	[DatAlt] datetime not null, 
-    CONSTRAINT [FK_Livro_To_Genero] FOREIGN KEY ([Genero]) REFERENCES [Generos]([Id]), 
-    CONSTRAINT [FK_Livro_To_Editora] FOREIGN KEY ([Editora]) REFERENCES [Editoras]([Id]),
-	CONSTRAINT [FK_Livro_To_UsuarioInc] FOREIGN KEY ([UsuInc]) REFERENCES [Usuarios]([Id]), 
-    CONSTRAINT [FK_Livro_To_UsuarioAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [Usuarios]([Id])
+﻿CREATE TABLE [dbo].[Livros] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Registro]    INT            NOT NULL,
+    [Titulo]      VARCHAR (1200) NOT NULL,
+    [Isbn]        VARCHAR (15)   NOT NULL,
+    [Genero]      INT            NOT NULL,
+    [Editora]     INT            NOT NULL,
+    [Sinopse]     NVARCHAR (MAX) NULL,
+    [Observaçoes] VARCHAR (1000) NULL,
+    [Ativo]       BIT            NOT NULL,
+    [UsuInc]      INT            NOT NULL,
+    [UsuAlt]      INT            NOT NULL,
+    [DatInc]      DATETIME       NOT NULL,
+    [DatAlt]      DATETIME       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Livro_To_Genero] FOREIGN KEY ([Genero]) REFERENCES [dbo].[Generos] ([Id]),
+    CONSTRAINT [FK_Livro_To_Editora] FOREIGN KEY ([Editora]) REFERENCES [dbo].[Editoras] ([Id]),
+    CONSTRAINT [FK_Livro_To_UsuarioInc] FOREIGN KEY ([UsuInc]) REFERENCES [dbo].[Usuarios] ([Id]),
+    CONSTRAINT [FK_Livro_To_UsuarioAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [dbo].[Usuarios] ([Id])
+);
 
-)

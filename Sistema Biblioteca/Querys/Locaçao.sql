1f485193008f6223854760b07452cc8fb5fb6445
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 10:13:02 -0300

    Sistema banco de dados da Biblioteca hbsis

---
+++ Sistema Biblioteca/Querys/Locaçao.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Locaçao] (
+    [Id]        INT      IDENTITY (1, 1) NOT NULL,
+    [Livro]     INT      NOT NULL,
+    [Usuario]   INT      NOT NULL,
+    [Tipo]      INT      NOT NULL,
+    [Devoluçao] DATETIME NOT NULL,
+    [Ativo]     BIT      NOT NULL,
+    [UsuInc]    INT      NOT NULL,
+    [UsuAlt]    INT      NOT NULL,
+    [DatInc]    DATETIME DEFAULT (getdate()) NOT NULL,
+    [DatAlt]    DATETIME DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Locaçao_Livros] FOREIGN KEY ([Livro]) REFERENCES [dbo].[Livros] ([Id]),
+    CONSTRAINT [FK_Locaçao_UsuarioAloc] FOREIGN KEY ([Usuario]) REFERENCES [dbo].[Usuarios] ([Id]),
+    CONSTRAINT [FK_Locaçao_UsuInc] FOREIGN KEY ([UsuInc]) REFERENCES [dbo].[Usuarios] ([Id]),
+    CONSTRAINT [FK_Locaçao_UsuAlt] FOREIGN KEY ([UsuAlt]) REFERENCES [dbo].[Usuarios] ([Id])
+);
+
